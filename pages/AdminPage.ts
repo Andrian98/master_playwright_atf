1b631edcd44bf6d69195af78306c932d7f2a817c
Andrian98 <andrian.moisei@endava.com>
2026-05-13 12:06:43 +0300

    Created a global-setup.ts and all options in the AdminPage.ts and now preconditions are executed before the main tests

--- pages/AdminPage.ts
+++ pages/AdminPage.ts
@@ -6,6 +6,17 @@ export class AdminPage {
     private readonly jmsStartupButton: Locator;
     private readonly databaseCleanedMessage: Locator;
     private readonly databaseInitializedMessage: Locator;
+    private readonly dataAccessModeJDBC: Locator;
+    private readonly webServiceSOAP: Locator;
+    private readonly webServiceRest: Locator;
+    private readonly loanProcessorService: Locator;
+    private readonly initBalance: Locator;
+    private readonly minBalance: Locator;
+    private readonly loanProvider: Locator;
+    private readonly loanProcessor: Locator;
+    private readonly threshold: Locator;
+    private readonly submitButton: Locator;
+    private readonly settingSavedMessage: Locator;
 
     constructor(page: Page) {
         this.initializeDatabaseButton = page.getByRole('button', {name: 'Initialize'});
@@ -13,9 +24,20 @@ export class AdminPage {
         this.jmsStartupButton = page.getByRole('button', {name: 'Startup'});
         this.databaseCleanedMessage = page.getByText('Database Cleaned');
         this.databaseInitializedMessage = page.getByText('Database Initialized');
+        this.dataAccessModeJDBC = page.locator('#accessMode4');
+        this.webServiceSOAP = page.locator('#soapEndpoint');
+        this.webServiceRest = page.locator('#restEndpoint');
+        this.loanProcessorService = page.locator('#endpoint');
+        this.initBalance = page.locator('#initialBalance');
+        this.minBalance = page.locator('#minimumBalance');
+        this.loanProvider = page.locator('#loanProvider');
+        this.loanProcessor = page.locator('#loanProcessor');
+        this.threshold = page.locator('#loanProcessorThreshold');
+        this.submitButton = page.getByRole('button', {name: 'Submit'});
+        this.settingSavedMessage = page.getByText('Settings saved successfully.');
     }
 
-    async initializeDatabase() {
+    async initializeAdminSetup() {
         await this.cleanDatabaseButton.click();
         await expect(this.databaseCleanedMessage).toBeVisible();
         await this.initializeDatabaseButton.click();
@@ -24,5 +46,28 @@ export class AdminPage {
         if (await this.jmsStartupButton.isVisible()) {
             await this.jmsStartupButton.click();
         }
+        await this.dataAccessModeJDBC.check();
+        await expect(this.dataAccessModeJDBC).toBeChecked();
+        await this.webServiceSOAP.clear();
+        await expect(this.webServiceSOAP).toHaveValue('');
+        await this.webServiceRest.clear();
+        await expect(this.webServiceRest).toHaveValue('');
+        await  this.loanProcessorService.clear();
+        await expect(this.loanProcessorService).toHaveValue('');
+        await this.initBalance.clear();
+        await this.initBalance.fill('5150.50');
+        await expect(this.initBalance).toHaveValue('5150.50');
+        await this.minBalance.clear();
+        await this.minBalance.fill('100.00');
+        await expect(this.minBalance).toHaveValue('100.00');
+        await this.loanProvider.selectOption('ws');
+        await expect(this.loanProvider).toHaveValue('ws');
+        await this.loanProcessor.selectOption('Available Funds');
+        await expect(this.loanProcessor).toHaveValue('funds');
+        await this.threshold.clear();
+        await this.threshold.fill('20');
+        await expect(this.threshold).toHaveValue('20');
+        await this.submitButton.click();
+        await expect(this.settingSavedMessage).toBeVisible();
     }
 }
--- tests/admin-setup.spec.ts
+++ tests/admin-setup.spec.ts
@@ -10,19 +10,12 @@ test.describe('Preconditions before test execution', () => {
     test('initialize admin database', async ({page}) => {
         const adminPage = new AdminPage(page);
         await page.goto(`${environment.baseUrl}${environment.adminPath}`);
-        await adminPage.initializeDatabase();
+        await adminPage.initializeAdminSetup();
 
         const registerPage = new RegisterPage(page);
         await page.goto(`${environment.baseUrl}${environment.registerPath}`);
         await registerPage.register(users.registerUser);
         await registerPage.validateSuccessfulRegistration(users.registerUser.username);
     });
-
-    // test('register user', async ({page}) => {
-    //     const registerPage = new RegisterPage(page);
-    //     await page.goto(`${environment.baseUrl}${environment.registerPath}`);
-    //     await registerPage.register(users.registerUser);
-    // });
 });
-
 
